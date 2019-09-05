023e4972c6399764872446a186e5590b99d616db
Ragnor Comerford <hello@ragnor.co>
2019-09-05 17:26:29 +0200

    Init.

---
+++ src/layouts/Default.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <header>
+      <nav class="navbar navbar-expand-lg @@classList">
+        <div class="container">
+          <!-- Brand -->
+          <g-link class="navbar-brand" to="/">
+            <img src="@/assets/img/logos/ucan-logo.png" class="navbar-brand-img" alt="..." />
+          </g-link>
+
+          <!-- Toggler -->
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <!-- Collapse -->
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <!-- Toggler -->
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarCollapse"
+              aria-controls="navbarCollapse"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <i class="fe fe-x"></i>
+            </button>
+
+            <!-- Navigation -->
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <g-link class="nav-link" id="navbarHome" to="/">Home</g-link>
+              </li>
+              <li class="nav-item">
+                <g-link class="nav-link" id="navbarHome" to="/about">About</g-link>
+              </li>
+              <li class="nav-item">
+                <g-link class="nav-link" id="navbarHome" to="/about">Services</g-link>
+              </li>
+              <li class="nav-item">
+                <g-link class="nav-link" id="navbarHome" to="/about">Bookings</g-link>
+              </li>
+              <li class="nav-item">
+                <g-link class="nav-link" id="navbarHome" to="/about">Blog</g-link>
+              </li>
+            </ul>
+
+            <!-- Button -->
+            <a
+              class="navbar-btn btn btn-sm btn-primary lift ml-auto"
+              href="@@webRoot/apply.html"
+            >Work with us</a>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <slot />
+    <footer class="py-8 py-md-11 @@classList">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-4 col-lg-3">
+            <!-- Brand -->
+            <img src="@/assets/img/logos/ucan-logo.png" alt="..." class="navbar-brand-img mb-2" />
+
+            <!-- Text -->
+            <p class="text-gray-700 mb-2">Maximising potential.</p>
+
+            <!-- Social -->
+            <ul class="list-unstyled list-inline list-social mb-6 mb-md-0">
+              <li class="list-inline-item list-social-item mr-3">
+                <a href="#!" class="text-decoration-none">
+                  <img
+                    src="@/assets/img/icons/social/instagram.svg"
+                    class="list-social-icon"
+                    alt="..."
+                  />
+                </a>
+              </li>
+              <li class="list-inline-item list-social-item mr-3">
+                <a href="#!" class="text-decoration-none">
+                  <img
+                    src="@/assets/img/icons/social/facebook.svg"
+                    class="list-social-icon"
+                    alt="..."
+                  />
+                </a>
+              </li>
+              <li class="list-inline-item list-social-item mr-3">
+                <a href="#!" class="text-decoration-none">
+                  <img
+                    src="@/assets/img/icons/social/twitter.svg"
+                    class="list-social-icon"
+                    alt="..."
+                  />
+                </a>
+              </li>
+              <li class="list-inline-item list-social-item">
+                <a href="#!" class="text-decoration-none">
+                  <img
+                    src="@/assets/img/icons/social/pinterest.svg"
+                    class="list-social-icon"
+                    alt="..."
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="col my-auto">
+            <div class="d-flex flex-row justify-content-center">
+              <div class="text-center mx-4">
+                <!-- Heading -->
+                <a href="javascript:tidioChatApi.open();">
+                  <h6 class="font-weight-bold text-uppercase text-gray-700">Contact & Help</h6>
+                </a>
+              </div>
+              <div class="text-center mx-4">
+                <!-- Heading -->
+                <h6 class="font-weight-bold text-uppercase text-gray-700">Terms & Conditions</h6>
+              </div>
+              <div class="text-center mx-4">
+                <!-- Heading -->
+                <h6 class="font-weight-bold text-uppercase text-gray-700">Privacy Policy</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- / .row -->
+      </div>
+      <!-- / .container -->
+    </footer>
+  </div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,250 @@
+<template>
+  <Layout>
+    <!-- WELCOME
+    ==================================================-->
+    <section class="pt-4 pt-md-11">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12 col-md-5 col-lg-6 order-md-2">
+            <!-- Image -->
+            <img
+              src="@/assets/img/illustrations/illustration-2.png"
+              class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0"
+              alt="..."
+              data-aos="fade-up"
+              data-aos-delay="100"
+            />
+          </div>
+          <div class="col-12 col-md-7 col-lg-6 order-md-1" data-aos="fade-up">
+            <!-- Heading -->
+            <h1 class="display-3 text-center text-md-left">
+              Personal statements made
+              <span class="text-primary">easy</span>.
+              <br />
+            </h1>
+
+            <!-- Text -->
+            <p class="lead text-center text-md-left text-muted mb-6 mb-lg-8">
+              Helping you achieve your full academic potential with a unique and personalised approach to UCAS
+              applications.
+            </p>
+
+            <!-- Buttons -->
+            <div class="text-center text-md-left">
+              <a href="@@webRoot/bookings.html" class="btn btn-primary shadow lift mr-1">
+                Get a free quote now
+                <i class="fe fe-arrow-right d-none d-md-inline ml-3"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- / .row -->
+      </div>
+      <!-- / .container -->
+    </section>
+
+    <!-- 
+    ==================================================-->
+    <section class="pt-8 pt-md-11" id="guidance">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-10 col-lg-8 text-center">
+            <!-- Badge -->
+            <span class="badge badge-pill badge-primary-soft mb-3">
+              <span class="h6 text-uppercase">1-on-1 guidance</span>
+            </span>
+
+            <!-- Heading -->
+            <h1>
+              <span class="text-primary">Impress</span> the admissions officers.
+            </h1>
+
+            <!-- Text -->
+            <p class="lead text-gray-700 mb-7 mb-md-9">
+              UCAN provides personalised 1-on-1 guidance from current undergraduates at top UK universities who have
+              successfully undergone the same application process. UCAN helps you to:
+            </p>
+          </div>
+        </div>
+        <!-- / .row -->
+        <div class="row align-items-center">
+          <div class="col-12 col-md-6 col-lg-7">
+            <!-- Screenshot -->
+            <div class="mb-8 mb-md-0">
+              <!-- Image -->
+              <img
+                src="@/assets/img/illustrations/illustration-7.png"
+                alt="..."
+                class="img-fluid mw-md-110 float-right mr-md-6 mb-6 mb-md-0"
+              />
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-5">
+            <div class="d-flex">
+              <!-- Badge -->
+              <div class="badge badge-rounded-circle badge-success-soft mt-1 mr-4">
+                <i class="fe fe-check"></i>
+              </div>
+
+              <!-- Text -->
+              <p class="text-gray-800">Reflect on your skills and experiences.</p>
+            </div>
+            <div class="d-flex">
+              <!-- Badge -->
+              <div class="badge badge-rounded-circle badge-success-soft mt-1 mr-4">
+                <i class="fe fe-check"></i>
+              </div>
+
+              <!-- Text -->
+              <p class="text-gray-800">Communicate your aptitudes, interests and accomplishments.</p>
+            </div>
+            <div class="d-flex">
+              <!-- Badge -->
+              <div class="badge badge-rounded-circle badge-success-soft mt-1 mr-4">
+                <i class="fe fe-check"></i>
+              </div>
+
+              <!-- Text -->
+              <p class="text-gray-800">Identify the most relevant information.</p>
+            </div>
+            <div class="d-flex">
+              <!-- Badge -->
+              <div class="badge badge-rounded-circle badge-success-soft mt-1 mr-4">
+                <i class="fe fe-check"></i>
+              </div>
+
+              <!-- Text -->
+              <p class="text-gray-800">Stay within the 4000-character limit.</p>
+            </div>
+            <div class="d-flex">
+              <!-- Badge -->
+              <div class="badge badge-rounded-circle badge-success-soft mt-1 mr-4">
+                <i class="fe fe-check"></i>
+              </div>
+
+              <!-- Text -->
+              <p class="text-gray-800">Fine-tune your statement by removing typos or grammar errors.</p>
+            </div>
+          </div>
+        </div>
+        <!-- / .row -->
+      </div>
+      <!-- / .container -->
+    </section>
+
+    <!-- Mentoring
+    ==================================================-->
+    <section class="py-8 py-md-12">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-12 col-md-6">
+            <!-- Badge -->
+            <span class="badge badge-pill badge-primary-soft mb-3">
+              <span class="h6 text-uppercase">Mentoring</span>
+            </span>
+
+            <!-- Heading -->
+            <h2>
+              Guidance from mentors at
+              <br />
+              <span class="text-primary">top UK universities</span>
+              .
+            </h2>
+
+            <!-- Text -->
+            <p class="font-size-lg text-gray-700 mb-6">
+              There is nothing more effective than feedback from a reliable mentor who has recently been in the same
+              position as you. They know how the system works and are here to help you through it.
+            </p>
+          </div>
+          <div class="col-12 col-md-6">
+            <!-- Image -->
+            <img
+              src="@/assets/img/illustrations/illustration-3.png"
+              alt="..."
+              class="img-fluid mw-md-130"
+            />
+          </div>
+        </div>
+        <!-- / .row -->
+      </div>
+      <!-- / .container -->
+    </section>
+
+    <!-- TESTIMONIALS
+    ==================================================-->
+    <section class="py-9 py-md-11 mb-9">
+      <div class="container">
+        <div class="row align-items-stretch">
+          <div class="col-12 col-md-5">
+            <!-- Blockquote -->
+            <blockquote class="blockquote text-center">
+              <!-- Logo -->
+
+              <!-- Text -->
+              <p class="mb-5 mb-md-7"></p>
+
+              <!-- Footer -->
+              <footer class="blockquote-footer mb-8 mb-md-0">
+                <span class="h6 text-uppercase">Aisha Q (Parent)</span>
+              </footer>
+            </blockquote>
+          </div>
+          <div class="col-12 col-md-1 border-right my-n8 my-md-n11 d-none d-md-block"></div>
+          <div class="col-12 col-md-5 offset-md-1">
+            <!-- Blockquote -->
+            <blockquote class="blockquote text-center">
+              <!-- Text -->
+              <p class="mb-5 mb-md-7"></p>
+              <!-- Footer -->
+              <footer class="blockquote-footer mb-0">
+                <span class="h6 text-uppercase">Jacob M. (Student)</span>
+              </footer>
+            </blockquote>
+          </div>
+        </div>
+        <!-- / .row -->
+      </div>
+      <!-- / .container -->
+    </section>
+
+    <!-- CTA
+    ==================================================-->
+    <section class="pt-6 pt-md-8 bg-gray-200">
+      <div class="container pb-6 pb-md-8 border-bottom border-gray-300">
+        <div class="row align-items-center">
+          <div class="col-12 col-md">
+            <!-- Heading -->
+            <h3 class="mb-1 mt-5 font-weight-bold">Perfect your personal statement today!</h3>
+
+            <!-- Text -->
+            <p class="font-size-lg text-muted mb-6 mb-md-0">
+              Get in touch with us and we will connect you with one of our top
+              mentors.
+            </p>
+          </div>
+          <div class="col-12 col-md-auto">
+            <!-- Button -->
+            <a href="bookings.html" class="btn btn-primary lift">Get in touch</a>
+          </div>
+        </div>
+        <!-- / .row -->
+      </div>
+      <!-- / .container -->
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  }
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
