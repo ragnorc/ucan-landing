4e345da77f40027c40b95e2c58df7f93505fcc66
Ragnor Comerford <hello@ragnor.co>
2020-08-26 17:26:42 +0400

    Add privacy,

---
+++ src/pages/Privacy.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <!-- WELCOME
+    ==================================================-->
+    <section class="py-8 py-md-11">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-10 col-lg-8 text-center">
+            <!-- Heading -->
+            <h1 class="display-2">Empowering students, maximising potential.</h1>
+
+            <!-- Text -->
+            <p
+              class="lead text-muted mb-7 mb-md-9"
+            >Universities and Colleges Applications Network (UCAN) is an educational consultancy that offers affordable personal statement guidance to students applying to UK universities.</p>
+          </div>
+        </div>
+        <!-- / .row -->
+
+
+    </section>
+  </Layout>
+</template>
+
+
+
+<script>
+export default {
+  metaInfo: {
+    title: "Privacy"
+  }
+};
+</script>
